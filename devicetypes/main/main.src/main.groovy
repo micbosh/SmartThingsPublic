2d17d3d060525428a741f5c7253ee0f74e4aff3c
Michael Shawky <micbosh2018@gmail.com>
2020-12-04 20:28:07 +1100

    one

---
+++ devicetypes/main/main.src/main.groovy
@@ -0,0 +1,219 @@
+/**
+ *  Main
+ *
+ *  Copyright 2020 Michael Shawky
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ */
+metadata {
+	definition (name: "Main", namespace: "Main", author: "Michael Shawky", cstHandler: true) {
+		capability "Alarm"
+		capability "Contact Sensor"
+		capability "Lock"
+		capability "Motion Sensor"
+		capability "Samsung TV"
+		capability "Security System"
+		capability "Step Sensor"
+		capability "Switch"
+		capability "Switch Level"
+		capability "TV"
+		capability "Tv Channel"
+	}
+
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'alarm' attribute
+	// TODO: handle 'contact' attribute
+	// TODO: handle 'lock' attribute
+	// TODO: handle 'motion' attribute
+	// TODO: handle 'volume' attribute
+	// TODO: handle 'mute' attribute
+	// TODO: handle 'pictureMode' attribute
+	// TODO: handle 'soundMode' attribute
+	// TODO: handle 'switch' attribute
+	// TODO: handle 'messageButton' attribute
+	// TODO: handle 'securitySystemStatus' attribute
+	// TODO: handle 'alarm' attribute
+	// TODO: handle 'steps' attribute
+	// TODO: handle 'goal' attribute
+	// TODO: handle 'switch' attribute
+	// TODO: handle 'level' attribute
+	// TODO: handle 'volume' attribute
+	// TODO: handle 'channel' attribute
+	// TODO: handle 'power' attribute
+	// TODO: handle 'picture' attribute
+	// TODO: handle 'sound' attribute
+	// TODO: handle 'movieMode' attribute
+	// TODO: handle 'tvChannel' attribute
+	// TODO: handle 'tvChannelName' attribute
+
+}
+
+// handle commands
+def off() {
+	log.debug "Executing 'off'"
+	// TODO: handle 'off' command
+}
+
+def strobe() {
+	log.debug "Executing 'strobe'"
+	// TODO: handle 'strobe' command
+}
+
+def siren() {
+	log.debug "Executing 'siren'"
+	// TODO: handle 'siren' command
+}
+
+def both() {
+	log.debug "Executing 'both'"
+	// TODO: handle 'both' command
+}
+
+def lock() {
+	log.debug "Executing 'lock'"
+	// TODO: handle 'lock' command
+}
+
+def unlock() {
+	log.debug "Executing 'unlock'"
+	// TODO: handle 'unlock' command
+}
+
+def volumeUp() {
+	log.debug "Executing 'volumeUp'"
+	// TODO: handle 'volumeUp' command
+}
+
+def volumeDown() {
+	log.debug "Executing 'volumeDown'"
+	// TODO: handle 'volumeDown' command
+}
+
+def setVolume() {
+	log.debug "Executing 'setVolume'"
+	// TODO: handle 'setVolume' command
+}
+
+def mute() {
+	log.debug "Executing 'mute'"
+	// TODO: handle 'mute' command
+}
+
+def unmute() {
+	log.debug "Executing 'unmute'"
+	// TODO: handle 'unmute' command
+}
+
+def setPictureMode() {
+	log.debug "Executing 'setPictureMode'"
+	// TODO: handle 'setPictureMode' command
+}
+
+def setSoundMode() {
+	log.debug "Executing 'setSoundMode'"
+	// TODO: handle 'setSoundMode' command
+}
+
+def on() {
+	log.debug "Executing 'on'"
+	// TODO: handle 'on' command
+}
+
+def off() {
+	log.debug "Executing 'off'"
+	// TODO: handle 'off' command
+}
+
+def showMessage() {
+	log.debug "Executing 'showMessage'"
+	// TODO: handle 'showMessage' command
+}
+
+def armStay() {
+	log.debug "Executing 'armStay'"
+	// TODO: handle 'armStay' command
+}
+
+def armAway() {
+	log.debug "Executing 'armAway'"
+	// TODO: handle 'armAway' command
+}
+
+def disarm() {
+	log.debug "Executing 'disarm'"
+	// TODO: handle 'disarm' command
+}
+
+def on() {
+	log.debug "Executing 'on'"
+	// TODO: handle 'on' command
+}
+
+def off() {
+	log.debug "Executing 'off'"
+	// TODO: handle 'off' command
+}
+
+def setLevel() {
+	log.debug "Executing 'setLevel'"
+	// TODO: handle 'setLevel' command
+}
+
+def volumeUp() {
+	log.debug "Executing 'volumeUp'"
+	// TODO: handle 'volumeUp' command
+}
+
+def volumeDown() {
+	log.debug "Executing 'volumeDown'"
+	// TODO: handle 'volumeDown' command
+}
+
+def channelUp() {
+	log.debug "Executing 'channelUp'"
+	// TODO: handle 'channelUp' command
+}
+
+def channelDown() {
+	log.debug "Executing 'channelDown'"
+	// TODO: handle 'channelDown' command
+}
+
+def setTvChannel() {
+	log.debug "Executing 'setTvChannel'"
+	// TODO: handle 'setTvChannel' command
+}
+
+def channelUp() {
+	log.debug "Executing 'channelUp'"
+	// TODO: handle 'channelUp' command
+}
+
+def channelDown() {
+	log.debug "Executing 'channelDown'"
+	// TODO: handle 'channelDown' command
+}
+
+def setTvChannelName() {
+	log.debug "Executing 'setTvChannelName'"
+	// TODO: handle 'setTvChannelName' command
+}
